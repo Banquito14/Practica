e68eecad6ee24451fb42da089a136ddc4e69533c
Banquito14 <leao.perez@tecsup.edu.pe>
2023-04-19 19:37:02 -0500

    nueva version

---
+++ base_de_datos.php
@@ -0,0 +1,19 @@
+<?php
+
+$contraseña = "contraseña_de_la_base_de_datos";
+$usuario = "usuario_de_la_base_de_datos";
+$nombreBaseDeDatos = "nombre_de_la_base_de_datos";
+
+$rutaServidor = "host_de_la_base_de_datos";
+$puerto = "puerto_de_la_base_de_datos";
+
+try{
+
+    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
+    $base_de_datos->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+} catch(Exception $e){
+    echo "Ocurrió un error con la base de datos: " . $e->getMessage();
+}
+
+?>
