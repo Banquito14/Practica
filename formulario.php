8835f183bf0187c30e4301d0df284e8ee614a3d2
Banquito14 <leao.perez@tecsup.edu.pe>
2023-04-20 00:07:20 -0500

    nueva version

--- formulario.php
+++ formulario.php
@@ -4,7 +4,7 @@
 ?>
 <?php include_once "encabezado.php"?>
 <div class="row">
-<div class="col-12">
+    <div class="col-12">
         <h1>Agregar</h1>
         <form action="insertar.php" method="POST">
             <div class="form-group">
@@ -20,5 +20,4 @@
         </form>
     </div>
 </div>
-
 <?php include_once "pie.php" ?>
