09f66996475a5e5a19cfc5542211cafbc91d6658
Banquito14 <leao.perez@tecsup.edu.pe>
2023-04-20 00:15:04 -0500

    nueva version

--- formulario.php
+++ formulario.php
@@ -1,7 +1,6 @@
 <?php
-
-
 ?>
+
 <?php include_once "encabezado.php"?>
 <div class="row">
     <div class="col-12">
@@ -15,9 +14,9 @@
                 <label for="edad">Edad</label>
                 <input required name="edad" type="number" id="edad" placeholder="Edad de mascota" class="form-control">
             </div>
-            <button type="submit" class="btn btn-succes">Guardar</button>
+            <button type="submit" class="btn btn-success">Guardar</button>
             <a href="./listar.php" class="btn btn-warning">Ver todas</a>
         </form>
     </div>
-</div>
+
 <?php include_once "pie.php" ?>
